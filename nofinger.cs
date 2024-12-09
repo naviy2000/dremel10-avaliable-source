34c0c7e1e106c5480c59620919487d199eae71bb
naviy2000 <ben@bendaws.net>
2024-12-09 10:44:47 -0600

    Create nofinger.cs

---
+++ nofinger.cs
@@ -0,0 +1,19 @@
+namespace naviy10.Mods {
+  public class Safety {
+    public static void NoFinger()
+    {
+            ControllerInputPoller.instance.leftControllerGripFloat = 0f;
+            ControllerInputPoller.instance.rightControllerGripFloat = 0f;
+            ControllerInputPoller.instance.leftControllerIndexFloat = 0f;
+            ControllerInputPoller.instance.rightControllerIndexFloat = 0f;
+            ControllerInputPoller.instance.leftControllerPrimaryButton = false;
+            ControllerInputPoller.instance.leftControllerSecondaryButton = false;
+            ControllerInputPoller.instance.rightControllerPrimaryButton = false;
+            ControllerInputPoller.instance.rightControllerSecondaryButton = false;
+            ControllerInputPoller.instance.leftControllerPrimaryButtonTouch = false;
+            ControllerInputPoller.instance.leftControllerSecondaryButtonTouch = false;
+            ControllerInputPoller.instance.rightControllerPrimaryButtonTouch = false;
+            ControllerInputPoller.instance.rightControllerSecondaryButtonTouch = false;
+    }
+  }
+}
