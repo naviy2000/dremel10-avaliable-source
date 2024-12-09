0bb905794861abfa6becdaf11e09ba48360ca6c9
naviy2000 <ben@bendaws.net>
2024-12-09 10:57:00 -0600

    Create iamnew.cs

---
+++ iamnew.cs
@@ -0,0 +1,19 @@
+namespace naviy10.Mods {
+  public class Safety {
+    public static void ChangeIdentity()
+        {
+            string randomName = "gorilla";
+            for (var i = 0; i < 4; i++)
+            {
+                randomName = randomName + UnityEngine.Random.Range(0, 9).ToString();
+            }
+
+            ChangeName(randomName);
+
+            byte randA = (byte)UnityEngine.Random.Range(0, 255);
+            byte randB = (byte)UnityEngine.Random.Range(0, 255);
+            byte randC = (byte)UnityEngine.Random.Range(0, 255);
+            ChangeColor(new Color32(randA, randB, randC, 255));
+        }
+  }
+}
