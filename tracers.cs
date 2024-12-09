4e17c9bc363c8c09a3703fe4a1e49bd22aa3f093
naviy2000 <ben@bendaws.net>
2024-12-09 11:09:22 -0600

    Create tracers.cs

---
+++ tracers.cs
@@ -0,0 +1,102 @@
+using System;
+using UnityEngine;
+using GorillaLocomotion;
+
+namespace naviy10.Mods {
+  public class Visual {
+    public static bool PlayerIsTagged(VRRig who)
+        {
+            string name = who.mainSkin.material.name.ToLower();
+            return name.Contains("fected") || name.Contains("it") || name.Contains("stealth") || !who.nameTagAnchor.activeSelf;
+            //return PlayerIsTagged(GorillaTagger.Instance.offlineVRRig);
+        }
+    
+    public static void CasualTracers()
+        {
+            float lineWidth = 0.025f;
+            foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+            {
+                if (vrrig != GorillaTagger.Instance.offlineVRRig)
+                {
+                    GameObject line = new GameObject("Line");
+                    //if (GetIndex("Hidden on Camera").enabled) { line.layer = 19; }
+                    LineRenderer liner = line.AddComponent<LineRenderer>();
+                    UnityEngine.Color thecolor = vrrig.playerColor;
+                    liner.startColor = thecolor; liner.endColor = thecolor; liner.startWidth = lineWidth; liner.endWidth = lineWidth; liner.positionCount = 2; liner.useWorldSpace = true;
+                    liner.SetPosition(0, GorillaTagger.Instance.rightHandTransform.position);
+                    liner.SetPosition(1, vrrig.transform.position);
+                    liner.material.shader = Shader.Find("GUI/Text Shader");
+                    UnityEngine.Object.Destroy(line, Time.deltaTime);
+                }
+            }
+        }
+
+        public static void InfectionTracers()
+        {
+            float lineWidth = 0.025f;
+            bool isInfectedPlayers = false;
+            foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+            {
+                if (PlayerIsTagged(vrrig))
+                {
+                    isInfectedPlayers = true;
+                    break;
+                }
+            }
+            if (isInfectedPlayers)
+            {
+                if (!PlayerIsTagged(GorillaTagger.Instance.offlineVRRig))
+                {
+                    foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+                    {
+                        if (PlayerIsTagged(vrrig) && vrrig != GorillaTagger.Instance.offlineVRRig)
+                        {
+                            GameObject line = new GameObject("Line");
+                            LineRenderer liner = line.AddComponent<LineRenderer>();
+                            UnityEngine.Color thecolor = new Color32(255, 111, 0, 255);
+                            liner.startColor = thecolor; liner.endColor = thecolor; liner.startWidth = lineWidth; liner.endWidth = lineWidth; liner.positionCount = 2; liner.useWorldSpace = true;
+                            liner.SetPosition(0, GorillaTagger.Instance.rightHandTransform.position);
+                            liner.SetPosition(1, vrrig.transform.position);
+                            liner.material.shader = Shader.Find("GUI/Text Shader");
+                            UnityEngine.Object.Destroy(line, Time.deltaTime);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+                    {
+                        if (!PlayerIsTagged(vrrig) && vrrig != GorillaTagger.Instance.offlineVRRig)
+                        {
+                            GameObject line = new GameObject("Line");
+                            LineRenderer liner = line.AddComponent<LineRenderer>();
+                            UnityEngine.Color thecolor = vrrig.playerColor;
+                            liner.startColor = thecolor; liner.endColor = thecolor; liner.startWidth = lineWidth; liner.endWidth = lineWidth; liner.positionCount = 2; liner.useWorldSpace = true;
+                            liner.SetPosition(0, GorillaLocomotion.Player.Instance.rightControllerTransform.position);
+                            liner.SetPosition(1, vrrig.transform.position);
+                            liner.material.shader = Shader.Find("GUI/Text Shader");
+                            UnityEngine.Object.Destroy(line, Time.deltaTime);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+                {
+                    if (vrrig != GorillaTagger.Instance.offlineVRRig)
+                    {
+                        GameObject line = new GameObject("Line");
+                        LineRenderer liner = line.AddComponent<LineRenderer>();
+                        UnityEngine.Color thecolor = vrrig.playerColor;
+                        liner.startColor = thecolor; liner.endColor = thecolor; liner.startWidth = lineWidth; liner.endWidth = lineWidth; liner.positionCount = 2; liner.useWorldSpace = true;
+                        liner.SetPosition(0, GorillaTagger.Instance.rightHandTransform.position);
+                        liner.SetPosition(1, vrrig.transform.position);
+                        liner.material.shader = Shader.Find("GUI/Text Shader");
+                        UnityEngine.Object.Destroy(line, Time.deltaTime);
+                    }
+                }
+            }
+        }
+  }
+}
